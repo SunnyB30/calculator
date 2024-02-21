d5730b26d12734f0a2f537143dc16ac0b1975e77
Sunil <146051989+SunnyB30@users.noreply.github.com>
2024-02-22 09:14:09 +1100

    Added event listeners to JS and basic CSS. Basic functions of calculator and GUI working.

--- calculator.js
+++ calculator.js
@@ -1,38 +1,132 @@
-operate(); 
+let userInput = []; 
+let operator = [];
+let numberStorage = [];
 
-function operate () {
+
+
+getNumbers();
+getOperator();
+getEquals();
+getClear(); 
+
+
+//Event listener functions
+
+function getNumbers () {
+
+    let buttons = document.querySelectorAll(".number");
     
-    let numberA = 1;
-    let numberB = 2;
-    let operator = "/";
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            userInput.push(button.value);
+            appendDisplay(userInput.join(""));
+        });
+    });
+
+}
+
+function getOperator () {
+    let buttons = document.querySelectorAll(".operator");
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            operator.push(button.value);
+
+            if (userInput.length) {
+                numberStorage.push(userInput.join(""));
+
+                if (numberStorage.length > 1) {
+                    let result = operate(parseInt(numberStorage.at(-2)), parseInt(numberStorage.at(-1)), operator.at(-2));
+                    appendDisplay(result);
+                    numberStorage.push(result); 
+                }
+            }
+            userInput = [];
+        });
+    });
+}
+
+function getEquals () {
+    let button = document.querySelector(".equals");
+
+    button.addEventListener('click', () => {
+        if (userInput.length) {
+            numberStorage.push(userInput.join(""));
+
+            if(numberStorage.length > 1) {
+                
+                let result = operate(parseInt(numberStorage.at(-2)), parseInt(numberStorage.at(-1)), operator.at(-1));
+                appendDisplay(result);
+                numberStorage.push(result);
+            }
+        }
+        userInput = []; 
+    });
+}
+
+function getClear () {
+
+    let button = document.querySelector(".clear");
+    button.addEventListener('click', () => {
+        userInput = []; 
+        operator = [];
+        numberStorage = [];
+        appendDisplay("");
+    });
+}
+
+//display functions
+
+function appendDisplay (update) {
+    document.querySelector(".display").textContent = update; 
+}
+
+
+//calculation functions 
+
+function operate (numberA, numberB, sign) {
+    
+    let operator = sign; 
     
     if (operator === "+") {
-        add(numberA, numberB);
+        return add(numberA, numberB);
     }
     else if (operator === "-") {
-        subtract(numberA, numberB);
+        return subtract(numberA, numberB);
     }
     else if (operator === "*") {
-        multiply(numberA, numberB);
+        return multiply(numberA, numberB);
     }
     else {
-        divide (numberA, numberB);
+        return divide (numberA, numberB);
     }
 }
 
 
 function add (numA, numB) {
-    console.log(numA + numB);
+    return numA + numB;
 }
 
 function subtract (numA, numB) {
-    console.log(numA - numB);
+    return numA - numB;
 }
 
 function multiply (numA, numB) {
-    console.log(numA * numB);
+    return numA * numB;
 }
 
 function divide (numA, numB) {
-    console.log(numA / numB);
+    return numA / numB;
 }
+
+
+
+
+
+
+
+
+
+
+
+
